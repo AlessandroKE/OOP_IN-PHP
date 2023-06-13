c3113a1cae21a0271061dea60f0c0d96d990952c
SandroKE <koomealessandro@gmail.com>
2023-06-14 02:26:52 +0300

    Creating a new class and Object

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+//defining a class 
+class User{
+    //All properties and methods are defined inside the class
+
+
+}
+
+//creting a new user and storing it in variable
+//creating a new instance of a class
+
+$sandro = new User();
+
+//function to determine which class an object is in  (get_class())
+
+echo 'This class is ' . get_class($sandro);
