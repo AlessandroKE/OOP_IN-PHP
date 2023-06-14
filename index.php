1d9d5d84e355c5e85cfff95e3686cbbe5107a64d
SandroKE <koomealessandro@gmail.com>
2023-06-14 03:16:55 +0300

    Functions for checking Variables and Methods in a class

--- index.php
+++ index.php
@@ -3,15 +3,30 @@
 //defining a class 
 class User{
     //All properties and methods are defined inside the class
+public $username = 'Alessandro';
+public $email = 'Alessandro@gmail.com';
 
-
+// creating a method inside the class
+public function addFreind(){
+    //$this-> used to refer to the current object
+    return "$this->username a new Guru has been created";
+}
 }
 
 //creting a new user and storing it in variable
 //creating a new instance of a class
 
 $sandro = new User();
-
+//The -> arrow operator is used to access a property or method of an object. 
+echo $sandro->username . "<br>";
+echo $sandro->email . "<br>";
+echo $sandro->addFreind() . "<br>";
 //function to determine which class an object is in  (get_class())
+//function to determine all variables available in the class (get_class_variable())
+//print_r(get_class_vars(User::class)) . '<br>';
+// Using get_class_vars()
+$classVariables = get_class_vars(User::class);
+$variableNames = array_keys($classVariables);
 
-echo 'This class is ' . get_class($sandro);
+echo 'These are the variables available: ' . implode(', ', $variableNames);
+//echo 'This class is ' . get_class($sandro);
