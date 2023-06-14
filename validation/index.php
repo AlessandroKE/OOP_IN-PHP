3c8077a698dd47d4eb32882c102f962b18bac3fa
SandroKE <koomealessandro@gmail.com>
2023-06-15 02:32:36 +0300

    updated index.php

---
+++ validation/index.php
@@ -0,0 +1,33 @@
+<?php 
+
+  if(isset($_POST['submit'])){
+    // validate entries
+    echo 'form submitted';
+  }
+
+?>
+
+<html lang="en">
+<head>
+  <title>PHP OOP</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+  
+  <div class="new-user">
+    <h2>Create a new user</h2>
+    <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
+
+      <label>username: </label>
+      <input type="text" name="username">
+
+      <label>email: </label>
+      <input type="text" name="email">
+
+      <input type="submit" value="submit" name="submit">
+
+    </form>
+  </div>
+
+</body>
+</html>
